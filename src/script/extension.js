6184d0e5ffb9d022110c6a91b676fd4a3a6d9a8b
fatihsen-dev <fatihydrm@outlook.com>
2024-09-26 23:12:14 +0300

    fix: f79e9e0ba71388a656e6d383fbe09a9de0a8df11

--- src/script/extension.js
+++ src/script/extension.js
@@ -2,38 +2,41 @@ const input = document.querySelector("#input");
 const inputLabel = document.querySelector("#inputLabel");
 
 if (typeof window !== null) {
-    window.onload = () => {
-        const value = hexToRgb(input.value);
-        inputLabel.style.background = `rgba(${value},1)`;
-        inputLabel.innerHTML = `rgba(${value},1)`;
-        send({
-            value,
-        });
+   window.onload = async () => {
+      let { value: defaultValue } = await chrome.storage.sync.get("value");
+      defaultValue = defaultValue ?? "#1f6feb";
+      input.value = defaultValue;
+      const value = hexToRgb(defaultValue);
+      inputLabel.style.background = `rgba(${value},1)`;
+      inputLabel.innerHTML = `rgba(${value},1)`;
+      send({
+         value,
+      });
 
-        input.addEventListener("input", (e) => {
-            const value = hexToRgb(e.target.value);
-            inputLabel.style.background = `rgba(${value},1)`;
-            inputLabel.innerHTML = `rgba(${value},1)`;
-            send({
-                value,
-            });
-            chrome.storage.sync.set({ value });
-        });
-    };
+      input.addEventListener("input", (e) => {
+         chrome.storage.sync.set({ value: e.target.value });
+         const value = hexToRgb(e.target.value);
+         inputLabel.style.background = `rgba(${value},1)`;
+         inputLabel.innerHTML = `rgba(${value},1)`;
+         send({
+            value,
+         });
+      });
+   };
 }
 
 async function send(message) {
-    const [tab] = await chrome.tabs.query({
-        active: true,
-        lastFocusedWindow: true,
-    });
-    await chrome.tabs.sendMessage(tab.id, message);
+   const [tab] = await chrome.tabs.query({
+      active: true,
+      lastFocusedWindow: true,
+   });
+   await chrome.tabs.sendMessage(tab.id, message);
 }
 
 function hexToRgb(value) {
-    const r = parseInt(value.substr(1, 2), 16);
-    const g = parseInt(value.substr(3, 2), 16);
-    const b = parseInt(value.substr(5, 2), 16);
+   const r = parseInt(value.substr(1, 2), 16);
+   const g = parseInt(value.substr(3, 2), 16);
+   const b = parseInt(value.substr(5, 2), 16);
 
-    return `${r}, ${g}, ${b}`;
+   return `${r}, ${g}, ${b}`;
 }
--- src/script/foreground.js
+++ src/script/foreground.js
@@ -1,25 +1,41 @@
 if (typeof window !== null) {
-    window.onload = () => {
-        const id = setInterval(async () => {
-            if (document.querySelector(".graph-before-activity-overview")) {
-                clearInterval(id);
-                const { value } = await chrome.storage.sync.get("value");
-                mercimek(value);
-            }
-        }, 100);
-    };
-}
-
-function mercimek(value = "255, 214, 0") {
-    const el = document.querySelector(".graph-before-activity-overview").style;
-    el.setProperty("--color-calendar-graph-day-L1-bg", `rgba(${value}, 0.25)`);
-    el.setProperty("--color-calendar-graph-day-L2-bg", `rgba(${value}, 0.5)`);
-    el.setProperty("--color-calendar-graph-day-L3-bg", `rgba(${value}, 0.75)`);
-    el.setProperty("--color-calendar-graph-day-L4-bg", `rgba(${value}, 1)`);
+   window.onload = () => {
+      const id = setInterval(async () => {
+         if (document.querySelector(".graph-before-activity-overview")) {
+            clearInterval(id);
+            let { value: defaultValue } = await chrome.storage.sync.get("value");
+            defaultValue = defaultValue ?? "#1f6feb";
+            mercimek(hexToRgb(defaultValue));
+         }
+      }, 100);
+   };
 }
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.value) {
-        mercimek(message.value);
-    }
+   if (message.value) {
+      mercimek(message.value);
+   }
 });
+
+function mercimek(value = "255, 214, 0") {
+   const pointsEl = document.querySelector(".graph-before-activity-overview").style;
+   pointsEl.setProperty("--color-calendar-graph-day-L1-bg", `rgba(${value}, 0.25)`);
+   pointsEl.setProperty("--color-calendar-graph-day-L2-bg", `rgba(${value}, 0.5)`);
+   pointsEl.setProperty("--color-calendar-graph-day-L3-bg", `rgba(${value}, 0.75)`);
+   pointsEl.setProperty("--color-calendar-graph-day-L4-bg", `rgba(${value}, 1)`);
+
+   const graphEl = document.querySelector(".js-activity-overview-graph-container");
+   graphEl.style.setProperty("--color-calendar-graph-day-L4-bg", `rgba(${value}, 1)`);
+
+   graphEl.querySelector(".js-highlight-blob").style.fill = `rgba(${value}, 1)`;
+   graphEl.querySelector(".js-highlight-blob").style.stroke = `rgba(${value}, 1)`;
+   graphPath.style.backgroundColor = "red !important";
+}
+
+function hexToRgb(value) {
+   const r = parseInt(value.substr(1, 2), 16);
+   const g = parseInt(value.substr(3, 2), 16);
+   const b = parseInt(value.substr(5, 2), 16);
+
+   return `${r}, ${g}, ${b}`;
+}
